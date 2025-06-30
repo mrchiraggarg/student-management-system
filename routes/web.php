c6ffc96f96cd7a9ec1a31f950a4c06b7c234421c
mrchiraggarg <gargchirag728@gmail.com>
2025-06-30 21:44:02 +0530

    Commit

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Models\Student;
 
-Route::get('/test', function () {
-    return Student::all(); // returns data from online MongoDB
-});
-
 Route::get('/students', [StudentController::class, 'index']);
 Route::post('/students', [StudentController::class, 'store']);
 Route::get('/students/{id}', [StudentController::class, 'show']);
