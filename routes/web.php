2eecc8ab5c5880c0c82396cd8ceb79192cfc7896
mrchiraggarg <gargchirag728@gmail.com>
2025-06-30 22:00:19 +0530

    Commit

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,5 @@ use App\Http\Controllers\StudentController;
 
 Route::get('/students', [StudentController::class, 'index']);
 Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'destroy']);
