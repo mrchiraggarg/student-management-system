b5ad8f4cc8de48a0b997b225871ca96a2815d6cc
mrchiraggarg <gargchirag728@gmail.com>
2025-06-30 16:03:17 +0530

    Commit

--- config/database.php
+++ config/database.php
@@ -16,18 +16,6 @@ return [
     |
     */
 
-    'mongodb' => [
-        'driver'   => 'mongodb',
-        'host'     => env('DB_HOST', '127.0.0.1'),
-        'port'     => env('DB_PORT', 27017),
-        'database' => env('DB_DATABASE'),
-        'username' => env('DB_USERNAME'),
-        'password' => env('DB_PASSWORD'),
-        'options'  => [
-            'database' => env('DB_AUTHENTICATION_DATABASE', 'admin') // required with MongoDB >= 3.0
-        ]
-    ],
-
     'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
@@ -42,6 +30,18 @@ return [
     */
 
     'connections' => [
+
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options'  => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin') // required with MongoDB >= 3.0
+            ]
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
