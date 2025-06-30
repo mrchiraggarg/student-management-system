ae80680a0e14ad30fd202e3e1a9022577b0f4136
mrchiraggarg <gargchirag728@gmail.com>
2025-06-30 15:49:14 +0530

    Commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    public function index() {
+        return Student::all();
+    }
+
+    public function store(Request $request) {
+        $data = $request->only(['name', 'email', 'course']);
+        return Student::create($data);
+    }
+
+    public function show($id) {
+        return Student::find($id);
+    }
+
+    public function update(Request $request, $id) {
+        $student = Student::findOrFail($id);
+        $student->update($request->only(['name', 'email', 'course']));
+        return $student;
+    }
+
+    public function destroy($id) {
+        return Student::destroy($id);
+    }
 }
