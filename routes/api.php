2679f9658bd0d0efdb7f36fc4fae6fa3fa2fc684
mrchiraggarg <gargchirag728@gmail.com>
2025-06-30 22:24:19 +0530

    Commit

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
