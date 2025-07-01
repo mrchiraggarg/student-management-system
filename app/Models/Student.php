c4a20bdc9875df4fcbbd9a0a9990246b04e2199b
mrchiraggarg <gargchirag728@gmail.com>
2025-07-01 11:52:36 +0530

    Commit

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,20 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class Student extends Model
 {
-    public function up()
-    {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->string('course')->nullable();
-            $table->timestamps();
-        });
-    }
+    //
 }
