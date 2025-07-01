839481e0afb947aee3d19a8208f5ca83abbaa5a0
mrchiraggarg <gargchirag728@gmail.com>
2025-07-01 11:59:49 +0530

    Commit

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,13 +1,13 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
-<h2>Edit Student</h2>
-<form action="{{ route('students.update', $id) }}" method="POST">
-    @csrf @method('PUT')
-    <label>Name: <input type="text" name="name" value="{{ $student['name'] }}" required></label><br>
-    <label>Email: <input type="email" name="email" value="{{ $student['email'] }}" required></label><br>
-    <label>Course: <input type="text" name="course" value="{{ $student['course'] }}" required></label><br>
-    <button type="submit">Update</button>
-</form>
-<a href="{{ route('students.index') }}">Back</a>
+<div class="container mt-4">
+    <h2>Edit Student</h2>
+    <form method="POST" action="{{ route('students.update', $student) }}">
+        @csrf @method('PUT')
+        @include('student.partials.form', ['student' => $student])
+        <button type="submit" class="btn btn-primary">Update</button>
+        <a href="{{ route('students.index') }}" class="btn btn-secondary">Back</a>
+    </form>
+</div>
 @endsection
