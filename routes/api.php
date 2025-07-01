e08f62601f0a0e8b3a118ebaa61599093c0f5307
mrchiraggarg <gargchirag728@gmail.com>
2025-07-01 12:02:52 +0530

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\StudentController;
 
-Route::get('/', [StudentController::class, 'index'])->name('students.index');
-Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
-Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
-Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
-Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+// Route::get('/', [StudentController::class, 'index'])->name('students.index');
+// Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+// Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+// Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+// Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+// Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
