aea19e4b6317c90cbbc0ca33cd8df9d515c334c0
mrchiraggarg <gargchirag728@gmail.com>
2025-07-01 12:18:26 +0530

    Commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
+Route::get('/', function () {
+    return redirect()->route('students.index');
+});
+
 Route::resource('students', StudentController::class);
 
 
